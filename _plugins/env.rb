b8e782ee7940e9eb8fc4815ca74e0d91abc9b7d7
Venkat Peri <venkatperi@gmail.com>
2017-08-10 10:21:46 -0400

    build number

--- _plugins/env.rb
+++ _plugins/env.rb
@@ -4,7 +4,7 @@ module Jekyll
     priority :highest
 
     def generate(site)
-      site.config['TRAVIS_BUILD_NUMBER'] = ENV['TRAVIS_NUMBER']  || 'n/a'
+      site.config['TRAVIS_BUILD_NUMBER'] = ENV['TRAVIS_BUILD_NUMBER']  || 'n/a'
       site.config['BUILD_TIME'] = Time.now()
       Jekyll.logger.info site.config.to_yaml
     end
